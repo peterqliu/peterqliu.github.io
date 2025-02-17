8ca2bdcb9e8eaf6f65517cde832e5c19a568b16b
germGame <peterqliu@gmail.com>
2025-02-17 09:14:40 -0600

    fix earthquake

--- pieces/credentials.js
+++ pieces/credentials.js
@@ -1,4 +1,5 @@
 var config = {
-	token: 'pk.eyJ1IjoicGV0ZXJhZnRlcm1hcGJveCIsImEiOiJjbHZuN3lmdHAwYTh5MnBtMmE5NWRpMWNjIn0.PvDSdFk3u8J_A69mFQOAaQ',
+	token: 'pk.eyJ1IjoicGV0ZXJxbGl1IiwiYSI6ImNqdHE0cXByZDAzaWY0NHBldG9yd3Jld28ifQ.8dISItctShjFnmnVeAgW2A',
     p: 'pk.eyJ1IjoicGV0ZXJxbGl1IiwiYSI6ImNqdHJqdG92OTBkNTg0M3BsNDY0d3NudWcifQ.0z4ov_viFE-yBMSijQpOhQ'
 };
+
